046feb23803db10cc857246e1bae635dc0ad3f69
Dr.Snowbird <DrSnowbird@openkbs.org>
2018-01-01 11:16:11 -0500

    update start up scripts Mon Jan  1 11:16:09 EST 2018

--- try-bind9-docker.sh
+++ try-bind9-docker.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 
-UPPER_DNS=127.0.0.1
+UPPER_DNS=192.168.0.1
+LOCAL_DNS=127.0.0.1
 DNS_IP=`ip route get 1|awk '{print$NF;exit;}'`
 
-docker run -d --name=bind --dns=${DNS_IP} \
-  --publish=${DNS_IP}:53:53/udp --publish=${DNS_IP}:10000:10000 \
+#   --publish=${DNS_IP}:53:53/udp --publish=${DNS_IP}:10000:10000 \
+
+
+## -- if CentOS or RedHat, to work with SeLinux,
+## -- you need to run these two commands
+
+mkdir -p /srv/docker/bind
+chcon -Rt svirt_sandbox_file_t /srv/docker/bind
+
+docker run -d --name=bind --dns=${UPPER_DNS} \
+  --publish=53:53/udp --publish=10000:10000 \
   --volume=/srv/docker/bind:/data \
   --env='ROOT_PASSWORD=password123' \
   sameersbn/bind:latest
